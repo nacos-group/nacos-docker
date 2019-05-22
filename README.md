600ce0509ac11f027a8f0b03c934a7837b84ed34
pader.zhang <pader.zhang@starlight-sms.com>
2019-05-22 16:18:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,99 @@
+# Nacos Docker
+
+![Docker Pulls](https://img.shields.io/docker/pulls/nacos/nacos-server.svg?maxAge=60480)
+
+This project contains a Docker image meant to facilitate the deployment of [Nacos](https://github.com/alibaba/nacos).
+
+
+## Project directory
+
+* build：Nacos makes the source code of the docker image
+* env: Environment variable file for compose yaml
+* example: Docker compose example for Nacos server
+
+
+## Quick Start
+
+Run the following command：
+
+* Clone project
+
+  ```powershell
+  git clone https://github.com/nacos-group/nacos-docker.git
+  cd nacos-docker
+  ```
+
+
+* Standalone Derby
+
+  ```powershell
+  docker-compose -f example/standalone-derby.yaml up
+  ```
+* Standalone Mysql
+
+  ```powershell
+  docker-compose -f example/standalone-mysql.yaml up
+  ```
+
+* Cluster
+
+  ```powershell
+  docker-compose -f example/cluster-hostname.yaml up 
+  ```
+
+
+* Service registration
+
+  ```powershell
+  curl -X PUT 'http://127.0.0.1:8848/nacos/v1/ns/instance?serviceName=nacos.naming.serviceName&ip=20.18.7.10&port=8080'
+  ```
+
+* Service discovery
+
+    ```powershell
+    curl -X GET 'http://127.0.0.1:8848/nacos/v1/ns/instances?serviceName=nacos.naming.serviceName'
+    ```
+
+* Publish config
+
+  ```powershell
+  curl -X POST "http://127.0.0.1:8848/nacos/v1/cs/configs?dataId=nacos.cfg.dataId&group=test&content=helloWorld"
+  ```
+
+* Get config
+
+  ```powershell
+    curl -X GET "http://127.0.0.1:8848/nacos/v1/cs/configs?dataId=nacos.cfg.dataId&group=test"
+  ```
+
+  
+
+* Open the Nacos console in your browser
+  
+  link：http://127.0.0.1:8848/nacos/
+
+
+
+
+## Common property configuration 
+
+| name                          | description                            | option                                 |
+| ----------------------------- | -------------------------------------- | -------------------------------------- |
+| MODE                          | cluster/standalone                     | cluster/standalone default **cluster** |
+| PREFER_HOST_MODE              | Whether hostname are supported         | hostname/ip default **ip**             |
+| NACOS_SERVER_PORT             | nacos server port                      | default **8848**                       |
+| NACOS_SERVER_IP             | custom nacos server ip when network was mutil-network                      |                         |
+| SPRING_DATASOURCE_PLATFORM    | standalone support mysql               | mysql / empty default empty            |
+| MYSQL_MASTER_SERVICE_HOST     | mysql master host                      |                                        |
+| MYSQL_MASTER_SERVICE_PORT     | mysql master database port             | default : **3306**                     |
+| MYSQL_MASTER_SERVICE_DB_NAME  | mysql master database name             |                                        |
+| MYSQL_MASTER_SERVICE_USER     | username of master database            |                                        |
+| MYSQL_MASTER_SERVICE_PASSWORD | password of master database            |                                        |
+| MYSQL_SLAVE_SERVICE_HOST      | mysql slave host                       |                                        |
+| MYSQL_SLAVE_SERVICE_PORT      | mysql slave database port              | default :3306                          |
+
+## Nacos + Grafana + Prometheus
+Usage reference：[Nacos monitor-guide](https://nacos.io/zh-cn/docs/monitor-guide.html)
+
+**Note**:  When Grafana creates a new data source, the data source address must be **http://prometheus:9090**
+ 
---
+++ build/bin/docker-startup.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Copyright 1999-2018 Alibaba Group Holding Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -x
+export DEFAULT_SEARCH_LOCATIONS="classpath:/,classpath:/config/,file:./,file:./config/"
+export CUSTOM_SEARCH_LOCATIONS=${DEFAULT_SEARCH_LOCATIONS},file:${BASE_DIR}/conf/,${BASE_DIR}/init.d/
+export CUSTOM_SEARCH_NAMES="application,custom"
+PLUGINS_DIR="/home/nacos/plugins/peer-finder"
+function print_servers(){
+   if [[ ! -d "${PLUGINS_DIR}" ]]; then
+    echo "" > "$CLUSTER_CONF"
+    for server in ${NACOS_SERVERS}; do
+            echo "$server" >> "$CLUSTER_CONF"
+    done
+   else
+    bash $PLUGINS_DIR/plugin.sh
+   sleep 30
+	fi
+}
+#===========================================================================================
+# JVM Configuration
+#===========================================================================================
+if [[ "${MODE}" == "standalone" ]]; then
+
+    JAVA_OPT="${JAVA_OPT} -Xms512m -Xmx512m -Xmn256m"
+    JAVA_OPT="${JAVA_OPT} -Dnacos.standalone=true"
+else
+
+  JAVA_OPT="${JAVA_OPT} -server -Xms2g -Xmx2g -Xmn1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m"
+  JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
+  JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
+  JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
+  print_servers
+fi
+
+#===========================================================================================
+# Setting system properties
+#===========================================================================================
+# set  mode that Nacos Server function of split
+if [[ "${FUNCTION_MODE}" == "config" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.functionMode=config"
+elif [[ "${FUNCTION_MODE}" == "naming" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.functionMode=naming"
+fi
+# set nacos server ip
+if [[ ! -z "${NACOS_SERVER_IP}" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.server.ip=${NACOS_SERVER_IP}"
+fi
+
+if [[ ! -z "${USE_ONLY_SITE_INTERFACES}" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.inetutils.use-only-site-local-interfaces=${USE_ONLY_SITE_INTERFACES}"
+fi
+
+if [[ ! -z "${PREFERRED_NETWORKS}" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.inetutils.preferred-networks=${PREFERRED_NETWORKS}"
+fi
+
+if [[ ! -z "${IGNORED_INTERFACES}" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.inetutils.ignored-interfaces=${IGNORED_INTERFACES}"
+fi
+
+if [[ "${PREFER_HOST_MODE}" == "hostname" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.preferHostnameOverIp=true"
+fi
+
+JAVA_MAJOR_VERSION=$($JAVA -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
+if [[ "$JAVA_MAJOR_VERSION" -ge "9" ]] ; then
+  JAVA_OPT="${JAVA_OPT} -Xlog:gc*:file=${BASE_DIR}/logs/nacos_gc.log:time,tags:filecount=10,filesize=102400"
+else
+  JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${JAVA_HOME}/lib/ext:${BASE_DIR}/plugins/cmdb"
+  JAVA_OPT="${JAVA_OPT} -Xloggc:${BASE_DIR}/logs/nacos_gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
+fi
+
+
+
+JAVA_OPT="${JAVA_OPT} -Dnacos.home=${BASE_DIR}"
+JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/target/nacos-server.jar"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+JAVA_OPT="${JAVA_OPT} --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
+JAVA_OPT="${JAVA_OPT} --spring.config.name=${CUSTOM_SEARCH_NAMES}"
+JAVA_OPT="${JAVA_OPT} --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
+
+echo "nacos is starting,you can check the ${BASE_DIR}/logs/start.out"
+echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/start.out 2>&1 &
+nohup $JAVA ${JAVA_OPT} > ${BASE_DIR}/logs/start.out 2>&1 < /dev/null
